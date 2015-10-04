8a7a66086d1b32b3645a69dc569cca6c3bf72198
Sergio <seanyabe@gmail.com>
2015-10-04 07:14:38 -0300

    fix0003

--- primes/primes.py
+++ primes/primes.py
@@ -9,3 +9,34 @@ def isPrime(val):
 			return False
 	return True
 
+def YaksicPrimes(val):
+	result=[]
+	for Pc in P:
+		aux=val-Pc
+		if aux < 2:
+			break
+		for Pb in P:
+			if aux % Pb ==0:
+				aux=aux/Pb
+				if aux < 2:
+					break
+				print aux
+				if isPrime(aux):
+					result.append(str(Pb)+'*'+str(aux)+'+'+str(Pc))
+	return result
+
+
+def generate(count):
+	for i in range(count):
+		new=N[len(N)-1]+1
+		N.append(new)
+		if isPrime(new):
+			P.append(new)
+	save="data={'N':"+str(N)+",'P':"+str(P)+"}"
+	f = open('data.py', 'w')
+	f.write(save)
+	return P
+
+
+
+
