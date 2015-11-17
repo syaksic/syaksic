39b0eff39deb500a5893750897ea0e11e07cb38c
syaksic <seanyabe@gmail.com>
2015-11-17 10:43:12 -0300

    lateUpdate

---
+++ python/juegoFabi/t1.py
@@ -0,0 +1,7 @@
+#python reconoce automaticamente el \n como "salto de linea en un string :P"
+
+txt="""
+Hola linda :D \n Esta es una tarea de progra XD \n srry tenia q hacerlo \n la gracia es que este texto tiene enters \n y necesitas hacer un programa en python para que al leerlo \n ponga los enters :P \n bueno eso XD te amoooooo
+"""
+
+print txt
